96dc775bb3911c7da350907fa0980d2faad227b0
timolubitz <lubitzti@cms.hu-berlin.de>
2014-11-28 15:41:48 +0100

    new features in SBML->SBtab conversion

--- SBMLConverter/sbml2sbtab.py
+++ SBMLConverter/sbml2sbtab.py
@@ -43,7 +43,7 @@ class SBMLDocument:
 
         sbtab_objects = []
         for sbtab in sbtabs:
-            new_tablib_obj = tablibIO.importSetNew(sbtab[0],'sbtab.tsv')
+            new_tablib_obj = tablibIO.importSetNew(sbtab[0],'sbtab.tsv',seperator='\t')
             single_tab     = SBtab.SBtabTable(new_tablib_obj,'sbtab.tsv')
             sbtab_objects.append(single_tab)
 
@@ -63,7 +63,9 @@ class SBMLDocument:
         '''
         build a Compartment SBtab
         '''
-        compartment_SBtab = '!!SBtab SBtabVersion="0.8" Document="'+self.filename.rstrip('.xml')+'" TableType="Compartment" TableName="Compartment"\n!Compartment\t!Name\t!Size\t!Unit\t!SBOTerm\n'
+        compartment_SBtab = '!!SBtab SBtabVersion="0.8" Document="'+self.filename.rstrip('.xml')+'" TableType="Compartment" TableName="Compartment"\n!Compartment\t!Name\t!Size\t!Unit\t!SBOTerm'
+        identifiers = []
+        the_rows    = ''
 
         for compartment in self.model.getListOfCompartments():
             value_row = compartment.getId()+'\t'
@@ -73,19 +75,34 @@ class SBMLDocument:
             except: value_row += '\t'
             try: value_row += str(compartment.getUnits())+'\t'
             except: value_row += '\t'           
-            if str(compartment.getSBOTerm()) == '-1': value_row += '\n'
-            else: str(compartment.getSBOTerm())+'\n'            
+            if str(compartment.getSBOTerm()) == '-1': value_row += '\t'
+            else: str(compartment.getSBOTerm())+'\t'            
             #try: value_row += str(compartment.getSBOTerm())+'\n'
             #except: value_row += '\t\n'
-            compartment_SBtab += value_row
 
+            try:
+                (annotation,urn) = self.getAnnotation(compartment)
+                if urn in identifiers: value_row += annotation+'\n'
+                else: value_row += '\n'
+                if not "!Identifiers:" in compartment_SBtab:
+                    compartment_SBtab += '\t'+'!Identifiers:'+urn
+                    identifiers.append(urn)
+            except:
+                value_row += '\n'
+            the_rows += value_row
+
+        compartment_SBtab += '\n'
+        compartment_SBtab += the_rows
+            
         return [compartment_SBtab,'compartment']
         
     def compoundSBtab(self):
         '''
         builds a Compound SBtab
         '''
-        compound_SBtab = '!!SBtab SBtabVersion="0.8" Document="'+self.filename.rstrip('.xml')+'" TableType="Compound" TableName="Compound"\n!Compound\t!Name\t!Location\t!Charge\t!IsConstant\t!SBOTerm\t!InitialConcentration\n'
+        compound_SBtab = '!!SBtab SBtabVersion="0.8" Document="'+self.filename.rstrip('.xml')+'" TableType="Compound" TableName="Compound"\n!Compound\t!Name\t!Location\t!Charge\t!IsConstant\t!SBOTerm\t!InitialConcentration'
+        identifiers = []
+        the_rows    = ''
 
         for species in self.model.getListOfSpecies():
             value_row = species.getId()+'\t'
@@ -98,28 +115,67 @@ class SBMLDocument:
             except: value_row += '\t'
             if str(species.getSBOTerm()) == '-1': value_row += '\t'
             else: str(species.getSBOTerm())+'\t'
-            #try: value_row += str(species.getSBOTerm())+'\t'
-            #except: value_row += '\t'
-            try: value_row += str(species.getInitialConcentration())+'\n'
-            except: value_row += '\t\n'
-            '''
-            dsds = species.getCVTerms()
-            for i in range(species.getNumCVTerms()):
-                cvterm = dsds.get(i)
-                print cvterm.getResourceURI(i)
-            try: value_row += species.getAnnotation()+'\n'
-            except: value_row += '\t\n'
-            '''
-            compound_SBtab += value_row
+            try: value_row += str(species.getInitialConcentration())+'\t'
+            except: value_row += '\t'
+
+            #this is a little bit more tricky: try and get an annotation from an element:
+            try:
+                (annotation,urn) = self.getAnnotation(species)
+                if urn in identifiers: value_row += annotation+'\n'
+                else: value_row += '\n'
+                if not "!Identifiers:" in compound_SBtab:
+                    compound_SBtab += '\t'+'!Identifiers:'+urn
+                    identifiers.append(urn)
+            except:
+                value_row += '\n'
+            the_rows += value_row
+
+        compound_SBtab += '\n'
+        compound_SBtab += the_rows
             
         return [compound_SBtab,'compound']
 
+
+    def getAnnotation(self,element):
+        '''
+        try and extract an annotation from an SBML element. PS: this is tricky stuff.
+        '''
+        cvterms    = element.getCVTerms()
+        annotation = False
+        urn        = False       
+
+        pattern2urn = {"CHEBI:\d+$":"obo.chebi",\
+                       "C\d+$":"kegg.compound",\
+                       "GO:\d{7}$":"obo.go",\
+                       "((S\d+$)|(Y[A-Z]{2}\d{3}[a-zA-Z](\-[A-Z])?))$":"obo.sgd",\
+                       "SBO:\d{7}$":"biomodels.sbo",\
+                       "\d+\.-\.-\.-|\d+\.\d+\.-\.-|\d+\.\d+\.\d+\.-|\d+\.\d+\.\d+\.(n)?\d+$":"ec-code",\
+                       "K\d+$":"kegg.orthology",\
+                       "([A-N,R-Z][0-9]([A-Z][A-Z, 0-9][A-Z, 0-9][0-9]){1,2})|([O,P,Q][0-9][A-Z, 0-9][A-Z, 0-9][A-Z, 0-9][0-9])(\.\d+)?$":"obo.uniprot"}#,\
+                       #"\d+$":"taxonomy"}
+
+        #for i in range(element.getNumCVTerms()):
+        cvterm   = cvterms.get(0)
+        resource = cvterm.getResourceURI(0)
+        #now try ALL the annotation identification patterns:
+        for pattern in pattern2urn.keys():
+            try:
+                search_annot = re.search(pattern,resource)
+                annotation   = search_annot.group(0)
+                urn = pattern2urn[pattern]
+                break
+            except: pass
+            
+        return (annotation,urn)
 
     def reactionSBtab(self):
         '''
         builds a Reaction SBtab
         '''
-        reaction_SBtab = '!!SBtab SBtabVersion="0.8" Document="'+self.filename.rstrip('.xml')+'" TableType="Reaction" TableName="Reaction"\n!Reaction\t!Name\t!SumFormula\t!Location\t!Modifier\t!KineticLaw\t!SBOTerm\t!IsReversible\n'
+        
+        reaction_SBtab = '!!SBtab SBtabVersion="0.8" Document="'+self.filename.rstrip('.xml')+'" TableType="Reaction" TableName="Reaction"\n!Reaction\t!Name\t!SumFormula\t!Location\t!Modifier\t!KineticLaw\t!SBOTerm\t!IsReversible'
+        identifiers = []
+        the_rows    = ''
 
         for reaction in self.model.getListOfReactions():
             value_row  = reaction.getId()+'\t'
@@ -149,9 +205,21 @@ class SBMLDocument:
             else: str(reaction.getSBOTerm())+'\t'
             #try: value_row += str(reaction.getSBOTerm())+'\t'
             #except: value_row += '\t'
-            try: value_row += str(reaction.getReversible())+'\n'
-            except: value_row += '\t\n'
-            reaction_SBtab += value_row
+            try: value_row += str(reaction.getReversible())+'\t'
+            except: value_row += '\t'
+            try:
+                (annotation,urn) = self.getAnnotation(reaction)
+                if urn in identifiers: value_row += annotation+'\n'
+                else: value_row += '\n'
+                if not "!Identifiers:" in reaction_SBtab:
+                    reaction_SBtab += '\t'+'!Identifiers:'+urn
+                    identifiers.append(urn)
+            except:
+                value_row += '\n'
+            the_rows += value_row
+
+        reaction_SBtab += '\n'
+        reaction_SBtab += the_rows
 
         return [reaction_SBtab,'reaction']
 
@@ -159,7 +227,9 @@ class SBMLDocument:
         '''
         builds a Quantity SBtab
         '''
-        quantity_SBtab = '!!SBtab SBtabVersion="0.8" Document="'+self.filename.rstrip('.xml')+'" TableType="Quantity" TableName="Quantity"\n!Quantity\t!SBML:parameter:id\t!Value\t!Unit\t!Description\n'
+        quantity_SBtab = '!!SBtab SBtabVersion="0.8" Document="'+self.filename.rstrip('.xml')+'" TableType="Quantity" TableName="Quantity"\n!Quantity\t!SBML:parameter:id\t!Value\t!Unit\t!Description'
+        identifiers = []
+        the_rows    = ''
 
         for reaction in self.model.getListOfReactions():
             kinetic_law = reaction.getKineticLaw()
@@ -171,8 +241,18 @@ class SBMLDocument:
                     value_row += str(parameter.getValue())+'\t'
                     try: value_row += parameter.getUnits()+'\t'
                     except: value_row += '\t'
-                    value_row += 'local parameter\t\n'
-                quantity_SBtab += value_row
+                    value_row += 'local parameter\t'
+                    #quantity_SBtab += value_row
+                    try:
+                        (annotation,urn) = self.getAnnotation(parameter)
+                        if urn in identifiers: value_row += annotation+'\n'
+                        else: value_row += '\n'
+                        if not "!Identifiers:" in quantity_SBtab:
+                            quantity_SBtab += '\t'+'!Identifiers:'+urn
+                            identifiers.append(urn)
+                    except:
+                        value_row += '\n'
+                the_rows += value_row
 
         for parameter in self.model.getListOfParameters():
             value_row = parameter.getId()+'\t'
@@ -180,8 +260,20 @@ class SBMLDocument:
             value_row += str(parameter.getValue())+'\t'            
             try: value_row += parameter.getUnits()+'\t'
             except: value_row += '\t'            
-            value_row += 'global parameter\t\n'
-            quantity_SBtab += value_row
+            value_row += 'global parameter\t'
+            try:
+                (annotation,urn) = self.getAnnotation(parameter)
+                if urn in identifiers: value_row += annotation+'\n'
+                else: value_row += '\n'
+                if not "!Identifiers:" in quantity_SBtab:
+                    quantity_SBtab += '\t'+'!Identifiers:'+urn
+                    identifiers.append(urn)
+            except:
+                value_row += '\n'
+            the_rows += value_row
+
+        quantity_SBtab += '\n'
+        quantity_SBtab += the_rows
             
         return [quantity_SBtab,'quantity']
 
