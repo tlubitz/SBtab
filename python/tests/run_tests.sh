5ef22c00e890245d4fa3fcf6f6078a8cf1c67502
Timo Lubitz <timo.lubitz@gmail.com>
2019-05-15 15:15:31 +0200

    [FIX] fixed the shell for automated tests

--- python/tests/run_tests.sh
+++ python/tests/run_tests.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 echo "Start shell script from root: (./python/tests/run_tests.sh)"
 
-python3.5 python/tests/test_sbtab.py
-python3.5 python/tests/test_sbtab_doc.py
-python3.5 python/tests/test_validator.py
-python3.5 python/tests/test_sbtab2sbml.py
-python3.5 python/tests/test_sbml2sbtab.py
-python3.5 python/tests/test_misc.py
-python3.5 python/tests/test_sbtab2html.py
+python python/tests/test_sbtab.py
+python python/tests/test_sbtab_doc.py
+python python/tests/test_validator.py
+python python/tests/test_sbtab2sbml.py
+python python/tests/test_sbml2sbtab.py
+python python/tests/test_misc.py
+python python/tests/test_sbtab2html.py
